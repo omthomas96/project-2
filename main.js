c6fb9730b4b00067fa316ddff261b5952e79d5f0
omthomas96 <omthomas@yahoo.com>
2025-07-09 19:22:31 -0400

    adding pagination

--- main.js
+++ main.js
@@ -11,6 +11,34 @@ searchInput.addEventListener("keypress", (event) => {
 
 const API_KEY = "DNTMN9dKFRLsqkl8nNp3gUFMdXmi6oLW";
 const API_ENDPOINT = "http://api.giphy.com/v1/gifs/search";
+let limit = 20;
+let offset = 0;
+let rating = "g";
+let totalCount = 0;
+let lastSearchTerm = "";
+
+// Pagination Buttons
+function setNavButtons() {
+  if (offset > 0) document.getElementById("prev").disabled = false;
+  else document.getElementById("prev").disabled = true;
+  if (totalCount > 0 && offset >= totalCount + limit)
+    document.getElementById("next").disabled = false;
+  else document.getElementById("next").disabled = true;
+}
+
+function handleClickPrev() {
+  if (offset >= limit) {
+    offset -= limit;
+    handleClickSearch();
+  }
+}
+
+function handleClickNext() {
+  if (offset <= totalCount + limit) {
+    offset += limit;
+    handleClickSearch();
+  }
+}
 
 //Call Function
 async function performSearch(event) {
@@ -29,7 +57,7 @@ async function performSearch(event) {
 // Fetch Function
 async function fetchData(data) {
   const fetchData = encodeURIComponent(data);
-  const url = `${API_ENDPOINT}?q=${fetchData}&api_key=${API_KEY}&limit=20&offset=0&rating=g`;
+  const url = `${API_ENDPOINT}?q=${fetchData}&api_key=${API_KEY}&limit=${limit}&offset=${offset}&rating=${rating}`;
 
   const response = await fetch(url);
   const json = await response.json();
@@ -48,4 +76,3 @@ function displayResults(response) {
 }
 
 // Pagination Function
-
