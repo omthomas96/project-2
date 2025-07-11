8cfb202511b7679ee89a5527cd6ca980ef8eb356
omthomas96 <omthomas@yahoo.com>
2025-07-11 19:03:30 -0400

    working on pagination

--- main.js
+++ main.js
@@ -39,7 +39,6 @@ async function performSearch(event) {
 async function fetchData(data) {
   const fetchData = encodeURIComponent(data);
   const url = `${API_ENDPOINT}?q=${fetchData}&api_key=${API_KEY}&limit=${limit}&offset=${offset}&rating=${rating}`;
-
   const response = await fetch(url);
   const json = await response.json();
   return json.data;
@@ -56,28 +55,63 @@ function displayResults(response) {
   }
 }
 
+// Image Function
+const imageUrls = [
+  "IMAGE_URL_1",
+  "IMAGE_URL_2",
+  "IMAGE_URL_3",
+  "IMAGE_URL_4",
+  "IMAGE_URL_5",
+  "IMAGE_URL_6",
+  "IMAGE_URL_7",
+  "IMAGE_URL_8",
+  "IMAGE_URL_9",
+  "IMAGE_URL_10",
+  "IMAGE_URL_11",
+  "IMAGE_URL_12",
+  "IMAGE_URL_13",
+  "IMAGE_URL_14",
+  "IMAGE_URL_15",
+  "IMAGE_URL_16",
+  "IMAGE_URL_17",
+  "IMAGE_URL_18",
+  "IMAGE_URL_19",
+  "IMAGE_URL_20",
+];
+
+function displayImageUrls(imageUrls) {
+  const resultsDiv = document.getElementById("results");
+  resultsDiv.innerHTML = "";
+  imageUrls.forEach((url) => {
+    const img = document.createElement("img");
+    img.src = url;
+    img.alt = "Giphy result";
+    img.onclick = () => displayImageUrls(imageUrls);
+    resultsDiv.appendChild(img);
+  });
+}
+
+displayImageUrls(imageUrls);
+
 // Pagination Function
 function setNavButtons() {
-  if (offset > 0) 
-    document.getElementById("prev").disabled = false;
-  else 
-    document.getElementById("prev").disabled = true;
-  if (totalCount > 0 && offset <= (totalCount + limit))
+  if (offset > 0) document.getElementById("prev").disabled = false;
+  else document.getElementById("prev").disabled = true;
+  if (totalCount > 0 && offset <= totalCount + limit)
     document.getElementById("next").disabled = false;
-  else 
-    document.getElementById("next").disabled = true;
+  else document.getElementById("next").disabled = true;
 }
 
 function handleClickPrev() {
   if (offset >= limit) {
     offset -= limit;
-    performSearch(new Event('click'));
+    performSearch(new Event("click"));
   }
 }
 
 function handleClickNext() {
-  if (offset <= (totalCount + limit)) {
+  if (offset <= totalCount + limit) {
     offset += limit;
-    performSearch(new Event('click'));
+    performSearch(new Event("click"));
   }
 }
